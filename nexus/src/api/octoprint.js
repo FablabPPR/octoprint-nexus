9ca16ebf176332c7058c34957721f79e76479746
Elie CHARRA <elie@elie.ski>
2020-03-04 20:26:25 +0000

    Add autoconnect

--- nexus/src/api/octoprint.js
+++ nexus/src/api/octoprint.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { get } from 'lodash'
+
 import config from '../config.json'
 
 const clients = {}
@@ -32,11 +34,56 @@ const getState = printer => {
 }
 
 export const getStatus = async printer => {
-    let status = {}
 
-    status.version = (await getVersion(printer)).data
-    status.settings = (await getSettings(printer)).data
-    status.state = (await getState(printer)).data.state
+    const status = {}
+
+    const version = getVersion(printer).then(response => status.version = response.data)
+    const settings = getSettings(printer).then(response => status.settings = response.data)
+    const state = getState(printer).then(response => status.state = response.data.state)
+
+    await Promise.all([version, settings, state]).then(values => {
+        status.version = values[0]
+        status.settings = values[1]
+        status.state = values[2]
+    });
+
+    let operational = get(status, 'state.flags.operational')
+    const retry = 10;
+    let retryCount = 0;
+
+    if (!operational) {
+        await connect(printer)
+    }
+
+    while(!operational) {
+        console.warn('Printer ' + printer + ' offline, try to reconnect, attempt ' + retryCount + '/ ' + retry + ' ...')
+        status.state = (await getState(printer)).data.state
+
+        operational = get(status, 'state.flags.operational')
+
+        if (operational) {
+            break
+        }
+
+        if (!operational) {
+            retryCount++;
+        }
+
+        if (retryCount > retry) {
+            break;
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 5000))
+    }
 
     return status
+}
+
+const connect = async printer => {
+    await getApi(printer).post('/connection', {
+        command: 'disconnect',
+    })
+    return getApi(printer).post('/connection', {
+        command: 'connect',
+    })
 }
